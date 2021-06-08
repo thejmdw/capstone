7181ad41328dd2d77d84eb08a0ba712a50881935
Jonathan Watson <thejmdw@gmail.com>
2021-06-08 00:49:47 -0400

    match cards rendering with some swipe

--- src/components/house/HouseList.js
+++ src/components/house/HouseList.js
@@ -1,8 +1,10 @@
 
 import React from "react"
 import { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import { HouseContext } from "./HouseProvider"
 import "./House.css"
+import TinderCard from "react-tinder-card"
 
 export const HouseList = () => {
   const { houses, getHouses } = useContext(HouseContext)
@@ -11,19 +13,34 @@ export const HouseList = () => {
     getHouses()
   }, [])
 
+  const history = useHistory()
+
+  const onSwipe = (d) => {
+    if (d === 'down') {
+      window.alert("You Swiped Left")
+    } else {
+      window.alert("You Swiped Right")
+    }
+  } 
+
   return (
     <>
-      <h3>Houses</h3>
-      <section>
+      <section className="houseCards__container">
         { houses.map((house) => {
           return (
-            <>
-            <img className="house_profilePic"src={house.photos[0].href}></img>
-            <div key={house.listing_id}>{house.listing_id}</div>
-            </>
+            <TinderCard className="swipe" key={house.property_id}  onSwipe={onSwipe} >
+              <div style={{backgroundImage: `url(${house.photos[0].href})`}} className="houseCard">
+                <h3>{house.address.line}</h3>
+                <h5>{house.property_id}</h5>
+              </div>
+            </TinderCard>
           )
         })}
       </section>
     </>
   )
 }
+            // <>
+            // <img className="house_profilePic"src={house.photos[0].href}></img>
+            // <div key={house.listing_id}>{house.listing_id}</div>
+            // </>
