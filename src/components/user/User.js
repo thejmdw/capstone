398442060ae1fd13748e6d7ec775b7c4ccb9be2c
Jonathan Watson <thejmdw@gmail.com>
2021-06-14 10:45:04 -0400

    profile search list sorted.

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -8,6 +8,7 @@ import TinderCard from "react-tinder-card"
 import Button from '@material-ui/core/Button';
 import { SearchContext } from "../search/SearchProvider"
 import { FaveContext } from "../fave/FaveProvider"
+import { ExposurePlus1 } from "@material-ui/icons"
 
 
 export const User = () => {
@@ -28,8 +29,8 @@ export const User = () => {
   }, [])
 
   const currentUser = profile
-  const currentUserSearches = searches
-  const history =useHistory()
+  const currentUserSearches = searches.sort((s1, s2) => (s1.id < s2.id ? 1 : -1))
+  const history = useHistory()
 
   const logOut = () => {
     localStorage.removeItem("swipeHome_user")
@@ -48,15 +49,19 @@ export const User = () => {
                     <h5>{currentUser.email}</h5>
                   </div>
                   <div className="test">
-                    <div>{`Total Faves: ${faves.length}`}</div>
-                    <Button onClick={() => {history.push("/faves")}}>Faves</Button>
+                    {/* <div>{`Total Faves: ${faves.length}`}</div> */}
+                    <Button onClick={() => {history.push("/faves")}}>Faves: {`${faves.length}`}</Button>
                   </div>
                 </div>
                 <div className="test">
                   <div>Last 5 Searches</div>
                   {currentUserSearches.length === 0 ? <div>You Haven't Searched for Anything Yet</div> :
-              
-                  <div>{`${currentUserSearches[0].city},${currentUserSearches[0].state_code} ${currentUserSearches[0].postal_code}`}
+                  <div>
+                  <div>{`1. ${currentUserSearches[0].city},${currentUserSearches[0].state_code} ${currentUserSearches[0].postal_code}`}</div>
+                  <div>{currentUserSearches[1] ? `2. ${currentUserSearches[1].city},${currentUserSearches[1].state_code} ${currentUserSearches[1].postal_code}`  : `2.`}</div>
+                  <div>{currentUserSearches[2] ? `3. ${currentUserSearches[2].city},${currentUserSearches[2].state_code} ${currentUserSearches[2].postal_code}` : `3.`}</div>
+                  <div>{currentUserSearches[3] ? `4. ${currentUserSearches[3].city},${currentUserSearches[3].state_code} ${currentUserSearches[3].postal_code}` : `4.`}</div>
+                  <div>{currentUserSearches[4] ? `5. ${currentUserSearches[4].city},${currentUserSearches[4].state_code} ${currentUserSearches[4].postal_code}` : `5. Sort This List`}</div>
                   </div>}
                 </div>
                 <div className="buttons">
