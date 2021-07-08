320d93578de7ce8c7abf865713334c54253a4e43
Jonathan Watson <thejmdw@gmail.com>
2021-07-07 21:47:12 -0400

    made app fit inside a 'mobile size' window

--- src/components/fave/FaveList.js
+++ src/components/fave/FaveList.js
@@ -61,7 +61,7 @@ export const FaveList = () => {
             <>
             <TinderCard className='swipe fave' preventSwipe={["up", "down"]} key={fave.id} onSwipe={(dir) => swiped(dir, fave.id)} onCardLeftScreen={() => outOfFrame(fave.property_id)}>
             <div style={{backgroundImage: `url(${fave.photo})`}} className="faveCard" onClick={() => {handleFaveClick(fave.id, fave.property_id)}}>
-                <div className="faveCardTitle" onClick={() => {handleFaveClick(fave.id, fave.property_id)}}>
+                <div className="faveCardTitle" onClick={() => {handleFaveClick(fave.id ? fave.id : fave.property_id)}}>
                 {/* <img  alt="house" className="faveList_housePic"/> */}
                 <div className="faveCard_address">
                   <h5>{fave.address} {fave.city}</h5>
