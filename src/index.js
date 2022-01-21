6e470701b5aa62b04bc1ac3d9356b2fa72d77cb2
Jonathan Watson <thejmdw@gmail.com>
2022-01-20 23:53:41 -0500

    firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,29 @@ import ReactDOM from 'react-dom';
 import { SwipeHome } from "./components/SwipeHome"
 import { BrowserRouter as Router } from "react-router-dom"
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMqQob-dFWlziWhJuOmZEo_zxZJckmY64",
+  authDomain: "swipehome-d73a6.firebaseapp.com",
+  databaseURL: "https://swipehome-d73a6-default-rtdb.firebaseio.com",
+  projectId: "swipehome-d73a6",
+  storageBucket: "swipehome-d73a6.appspot.com",
+  messagingSenderId: "877328288748",
+  appId: "1:877328288748:web:ac065cc9cbd55fa62f4ec9",
+  measurementId: "G-HS5NE725R3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
